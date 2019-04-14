faa0cfc48e71c59247a7158fd59953865c699b98
TimChien112 <wech1172@colorado.edu>
2019-04-13 19:22:14 -0600

    add header file

--- source code/source/application.c
+++ source code/source/application.c
@@ -3,7 +3,7 @@
 *	@brief 		application codes for this project
 *	
 *
-*	@author 		
+*	@author 		Linfeng Li
 *	@date 			March 30 2019 
 *	@version  	1.0
 */
@@ -20,40 +20,44 @@ extern volatile bool RxRingBufferFullWarning;
 extern volatile uint32_t tick;
 extern uint32_t seed;
 uint32_t report_cnt[256];
-static char* sp_char_lookuptable[]={"NUL","SOH","STX","ETX","EOT","ENQ","ACK","BEL","BS","TAB",
+static char* sp_char_lookuptable[]={"NUL","SOH","STX","ETX","EOT","ENQ","ACK","BEL","BS","TAB",			//all special characters in ASCII table
 		"LF","VT","FF","CR","SO","SI","DLE","DC1","DC2","DC3","DC4",
 		"NAK","SYN","ETB","CAN","EM ","SUB","ESC","FS","GS","RS","US","SPACE","DEL"};
 
+		
+/**
+*	@brief 		state machine for report operation  
+*/
 void application(void){
 	static state current_state = UPDATE_REPORT;
 	static uint8_t character;
 	static uint32_t updated_cnt = 0;
 	static int32_t status;
-	switch(current_state){
-		case UPDATE_REPORT:
-			if(!first_itr_update_report){
+	switch(current_state){				
+		case UPDATE_REPORT:									
+			if(!first_itr_update_report){				
 				first_itr_update_report=1;
-				empty_before_first_itr_update_report = (entries(RxRingBuffer)==0)? true : false;
+				empty_before_first_itr_update_report = (entries(RxRingBuffer)==0)? true : false;	//check the ring_buffer is vacant
 				if(empty_before_first_itr_update_report){
 					wait_for_new_char = true;
 					break;
 				}
 			}
-			status = extract(RxRingBuffer,&character);
-			if(status != RING_EMPTY){
+			status = extract(RxRingBuffer,&character);					//extract a character from ring buffer
+			if(status != RING_EMPTY){									//if any data remained in ring buffer
 				wait_for_new_char = false;
 				report_cnt[(uint32_t)character]++;
 				updated_cnt++;
 			}
 			if( ((tick >TIMEOUT_TICK) &&  !wait_for_new_char) || (updated_cnt>=MAX_REPORT_CHAR) )//||  ((status ==RING_EMPTY) && !wait_for_new_char)  )
-				current_state = WAIT_FOR_PREVIOUS_TX;
+				current_state = WAIT_FOR_PREVIOUS_TX;					//move state machine to next state if 
 			break;
-		case WAIT_FOR_PREVIOUS_TX:
+		case WAIT_FOR_PREVIOUS_TX:										
 			first_itr_update_report = false;
 			empty_before_first_itr_update_report = false;
 			wait_for_new_char = false;
 			updated_cnt = 0;
-			if(!UART_NONBLOCKING_TX_BUSY(OPENSDA_UART))
+			if(!UART_NONBLOCKING_TX_BUSY(OPENSDA_UART))					//wait until previous transfer complete
 				current_state = GENERATE_REPORT;
 			break;
 		case GENERATE_REPORT:
@@ -63,7 +67,12 @@ void application(void){
 			break;
 	}
 }
-
+/**
+*	@brief 		generate a character count report 
+*	
+*	@param		uint32_t*	report_cnt		array of count for all characters in ASCII table (0~255)	
+*	
+*/
 void report_generation(ring_t* ring, uint32_t* report_cnt){
 	int32_t num;
 	char temp_buffer[20];
@@ -76,12 +85,12 @@ void report_generation(ring_t* ring, uint32_t* report_cnt){
 	ring_block_insert(ring,(uint8_t*)temp_buffer,num);
 	num = sprintf(temp_buffer,"%d\n",seed);
 	ring_block_insert(ring,(uint8_t*)temp_buffer,num);
-	if(TxRingBufferFullWarning){
+	if(TxRingBufferFullWarning){										// if transfer buffer is full
 		TxRingBufferFullWarning=false;
 		num = sprintf(temp_buffer,"TxRingBufferFullWarning\n");
 		ring_block_insert(ring,(uint8_t*)temp_buffer,num);
 	}
-	if(RxRingBufferFullWarning){
+	if(RxRingBufferFullWarning){										// if received buffer is full
 		RxRingBufferFullWarning=false;
 		num = sprintf(temp_buffer,"RxRingBufferFullWarning\n");
 		ring_block_insert(ring,(uint8_t*)temp_buffer,num);
@@ -90,19 +99,19 @@ void report_generation(ring_t* ring, uint32_t* report_cnt){
 	for(int i=0;i<256;i++){
 		if(report_cnt[i]==0)
 			continue;
-		if((i>=0) && (i<=32)){
+		if((i>=0) && (i<=32)){											// index i => special character (0~32) in ASCII code
 			special_char = sp_char_lookuptable[i];
 			num = sprintf(temp_buffer,"%s-%d\n",special_char,report_cnt[i]);
 		}
 		else if(i==127){
-			special_char = sp_char_lookuptable[33];
+			special_char = sp_char_lookuptable[33];						// index 127 => special character (DEL) in ASCII code
 			num = sprintf(temp_buffer,"%s-%d\n",special_char,report_cnt[i]);
 		}
 		else if(i<127){
-			num = sprintf(temp_buffer,"%c-%d\n",i,report_cnt[i]);
+			num = sprintf(temp_buffer,"%c-%d\n",i,report_cnt[i]);		// index i => printable character (33~126) in ASCII code
 		}
 		else{
-			num = sprintf(temp_buffer,"VAL:%d-%d\n",i,report_cnt[i]);
+			num = sprintf(temp_buffer,"VAL:%d-%d\n",i,report_cnt[i]);	// index i => extended characters (127~255) in ASCII code
 		}
 		if(ring_block_insert(ring,(uint8_t*)temp_buffer,num) == RING_FULL){
 			TxRingBufferFullWarning=true;
@@ -114,9 +123,16 @@ void report_generation(ring_t* ring, uint32_t* report_cnt){
 
 }
 
-
-
-
+/**
+*	@brief 		insert a character array into ring buffer 
+*	
+*	@param		uint8_t*	data			the data array need to be inserted	
+*				uint32_t* 	length			number of the data in data array			
+*	
+*	@return		0	if insert data successfully 
+*				-1 	if input with NULL pointer to ring_t 
+*				-2 	if buffer is already full
+*/
 uint32_t ring_block_insert(ring_t* ring, uint8_t* data, uint32_t length){
 	uint32_t status;
 	if(data==NULL)
@@ -130,6 +146,11 @@ uint32_t ring_block_insert(ring_t* ring, uint8_t* data, uint32_t length){
 	return status;
 }
 
+/**
+*	@brief 		 pseudo random generator
+*	
+*	@param		uint8_t*	buffer for random number return				
+*/
 void PRG_data(uint32_t* data){
 	if(data==NULL)
 		return;
